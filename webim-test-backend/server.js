0902ff4625d9701a1882fe19f4641b615f4960a6
Boltunov Artem <tema2000.28@gmail.com>
2019-07-01 21:19:17 +0300

    Update

---
+++ webim-test-backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const https = require('https');
+
+let app = express();
+let passport = require('passport');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors());
+
+app.get('/auth/vk', function(req, res){
+	res.redirect('https://oauth.vk.com/authorize?' +
+		'client_id=7040403&' +
+		'display=page&' +
+		'redirect_uri=http://bolart.ru/auth/vkontakte/callback&scope=friends&' +
+		'response_type=code');
+});
+
+app.get('/auth/vkontakte/callback', function(req, res) {
+
+	// https.get(url, (resp) => {
+	//
+	// });
+});
+
+app.listen(3000);
