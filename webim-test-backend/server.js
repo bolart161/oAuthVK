fada9d62f75bc1b795ea54303ce431f066aec829
Boltunov Artem <tema2000.28@gmail.com>
2019-07-01 22:16:06 +0300

    123

--- webim-test-backend/server.js
+++ webim-test-backend/server.js
@@ -12,25 +12,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.get('/auth/vk', function(req, res){
-	res.redirect('https://oauth.vk.com/authorize?' +
+	let url = 'https://oauth.vk.com/authorize?' +
 		'client_id=7040403&' +
 		'display=page&' +
 		'redirect_uri=http://bolart.ru:3000/auth/vkontakte/callback&scope=friends&' +
-		'response_type=code');
+		'response_type=code';
+	axios.get(url)
+		.then((response) => {
+			console.log( response );
+		})
+		.then((data) => {
+
+		})
+		.catch(console.log('Error'));
+
 });
 
 app.get('/auth/vkontakte/callback', cors(), function(req, res) {
 	const code = req.query.code,
 		url = 'https://oauth.vk.com/access_token?client_id=7040403&client_secret=SJrmcvaarjNDIJnRm7qe&redirect_uri=http://bolart.ru:3000/auth/vkontakte/callback&code=' + code;
 
-	// if (code !== undefined) {
-	// 	console.log(code);
-	// 	res.redirect(url);
-	// }
 	axios.get(url)
 		.then((response) => {
-			console.log(response.data);
-			console.log(response);
+			return response.data;
+		})
+		.then((data) => {
+
 		})
 		.catch(console.log('Error'));
 
