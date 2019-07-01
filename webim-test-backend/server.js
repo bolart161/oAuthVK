042bacf482d5e8ddb29a664c622a4e3d7c9d1c35
Boltunov Artem <tema2000.28@gmail.com>
2019-07-01 22:39:58 +0300

    ff

--- webim-test-backend/server.js
+++ webim-test-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 let app = express();
 let passport = require('passport');
