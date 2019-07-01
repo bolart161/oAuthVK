30262a136419df28eba27862044517c323ed034b
Boltunov Artem <tema2000.28@gmail.com>
2019-07-01 19:13:47 +0300

    Update

--- webim-test-front/src/components/AuthButton.vue
+++ webim-test-front/src/components/AuthButton.vue
@@ -18,7 +18,7 @@
 <script type="text/javascript">
 function getToken(code) {
   let script = document.createElement('SCRIPT');
-  script.src = 'https://oauth.vk.com/access_token?client_id=6079378&client_secret=VZjMf75BX1zNY7Avn6D1&redirect_uri=http://bolart.ru&code=' + code + '&callback=saveToken';
+  script.src = 'https://oauth.vk.com/access_token?client_id=7040403&client_secret=SJrmcvaarjNDIJnRm7qe&redirect_uri=http://bolart.ru&code=' + code + '&callback=saveToken';
   script.type = 'text/javascript';
   document.getElementsByTagName("head")[0].appendChild(script);
 }
@@ -40,8 +40,8 @@ export default {
   methods: {
     auth() {
       let urlAuth = 'https://oauth.vk.com/authorize?' +
-          'client_id=6079378&' +
-          'display=popup&' +
+          'client_id=7040403&' +
+          'display=page&' +
           'redirect_uri=http://bolart.ru&scope=friends&' +
           'response_type=code';
 
